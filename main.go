c3a323df25c6b077fca26caf9e1fe690ffc51451
feruza17 <feruzamirjalilova767@gmail.com>
2024-06-25 15:39:31 +0500

    .

--- main.go
+++ main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/casbin/casbin/v2"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/casbin/casbin/v2"
 )
+
+type Subject struct {
+	Role string
+}
+
+type Object struct {
+	Type string
+}
+
+type Env struct {
+	Time         string
+	FaceID       string
+	EnoughPoints string
+	PaymentDone  string
+	LessonNumber string
+}
+
+type Action struct {
+	Action string
+}
 
 func main() {
 	er, err := casbin.NewEnforcer("model.conf", "policy.csv")
@@ -22,7 +43,7 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("Choose mode: (1) RBAC (2) ABAC (3) Exit")
+		fmt.Println("[1] RBAC [2] ABAC [0] Exit")
 		mode, _ := reader.ReadString('\n')
 		mode = strings.TrimSpace(mode)
 
@@ -35,7 +56,7 @@ func main() {
 			fmt.Println("Exiting...")
 			return
 		default:
-			fmt.Println("Invalid choice, please try again.")
+			fmt.Println("Invalid choice, please try again")
 		}
 	}
 }
@@ -71,11 +92,35 @@ func runABACCLI(e *casbin.Enforcer) {
 	act, _ := reader.ReadString('\n')
 	act = strings.TrimSpace(act)
 
-	fmt.Print("Enter environment attribute (time): ")
+	fmt.Print("Enter environment attribute Time: ")
 	t, _ := reader.ReadString('\n')
 	t = strings.TrimSpace(t)
 
-	PermissionABAC(e, sub, obj, act, t)
+	fmt.Print("Enter environment attribute FaceID: ")
+	face_id, _ := reader.ReadString('\n')
+	face_id = strings.TrimSpace(face_id)
+
+	fmt.Print("Enter environment attribute EnoughPoints: ")
+	point, _ := reader.ReadString('\n')
+	point = strings.TrimSpace(point)
+
+	fmt.Print("Enter environment attribute PaymentDone: ")
+	payment, _ := reader.ReadString('\n')
+	payment = strings.TrimSpace(payment)
+
+	fmt.Print("Enter environment attribute LessonNumber: ")
+	lesson, _ := reader.ReadString('\n')
+	lesson = strings.TrimSpace(lesson)
+
+	env := Env{
+		Time:         t,
+        FaceID:       face_id,
+        EnoughPoints: point,
+        PaymentDone:  payment,
+        LessonNumber: lesson,
+    }
+
+	PermissionABAC(e, sub, obj, act, env)
 }
 
 func checkPermissionRBAC(e *casbin.Enforcer, sub, obj, act string) {
@@ -91,16 +136,15 @@ func checkPermissionRBAC(e *casbin.Enforcer, sub, obj, act string) {
 	}
 }
 
-func PermissionABAC(e *casbin.Enforcer, sub, obj, act, t string) {
-	ok, err := e.Enforce(sub, obj, act, t)
+func PermissionABAC(e *casbin.Enforcer, sub, obj, act string, env Env) {
+	ok, err := e.Enforce(sub, obj, act, env.Time, env.FaceID, env.EnoughPoints, env.PaymentDone, env.LessonNumber)
 	if err != nil {
 		log.Fatalf("Failed to enforce policy: %v", err)
 	}
 
 	if ok {
-		fmt.Printf("Access granted for %s to %s %s at %s %s\n", sub.Role, act, obj.DataType, env.TimeOfDay, env.Location)
+		fmt.Printf("Access granted for %s to %s %s at %s\n", sub, act, obj, env)
 	} else {
-		fmt.Printf("Access denied for %s to %s %s at %s %s\n", sub.Role, act, obj.DataType, env.TimeOfDay, env.Location)
+		fmt.Printf("Access denied for %s to %s %s at %s\n", sub, act, obj, env)
 	}
 }
-
